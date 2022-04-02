e707c6af7d892b27cc37f5bc5380777466b9dee1
srahman02 <rahmansojibur05@gmail.com>
2022-04-02 13:15:16 -0400

    fixed failing service test and added negative test

--- src/test/groovy/com/example/moviecatalogservice/service/CatalogServiceSpec.groovy
+++ src/test/groovy/com/example/moviecatalogservice/service/CatalogServiceSpec.groovy
@@ -1,5 +1,7 @@
 package com.example.moviecatalogservice.service
 
+import com.example.moviecatalogservice.exception.ItemsNotFoundException
+import com.example.moviecatalogservice.gateway.MovieRestGateway
 import com.example.moviecatalogservice.model.CatalogItem
 import com.example.moviecatalogservice.model.CatalogItems
 import com.example.moviecatalogservice.service.impl.CatalogServiceImpl
@@ -7,21 +9,34 @@ import spock.lang.Specification
 
 class CatalogServiceSpec extends Specification {
     CatalogService catalogService
+    MovieRestGateway mockGatewayService = Mock()
 
     def setup() {
-        catalogService = new CatalogServiceImpl()
+        catalogService = new CatalogServiceImpl(mockGatewayService)
     }
 
     def "should return a list of catalog when called with a userId"() {
         given:
         def userId = "1"
         def catalogItem = [name: "Transformer", desc: "Transformer Description", rating: 4.0] as CatalogItem
-        def catalogItems = new CatalogItems()
-        catalogItems.catalogItems = [catalogItem]
+        def expectedItems = new CatalogItems()
+        expectedItems.catalogItems = [catalogItem]
 
         when:
         CatalogItems actualItems = catalogService.getCatalogItems(userId)
         then:
-        actualItems == catalogItems
+        1 * mockGatewayService.getCatalogItems(userId) >> expectedItems
+        actualItems == expectedItems
+    }
+
+    def "should throw ItemNotFoundException if the gateway call fails with 404"() {
+        given:
+        def userId = "1"
+
+        when:
+        catalogService.getCatalogItems(userId)
+        then:
+        1 * mockGatewayService.getCatalogItems(userId) >> { throw new ItemsNotFoundException() }
+        thrown ItemsNotFoundException
     }
 }
